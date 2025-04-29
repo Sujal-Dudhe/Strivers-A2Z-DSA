05341bda73943acf53dffd4936d628847853b078
Sujal Dudhe <sujaldudhe2004@gmail.com>
2025-04-29 16:59:48 +0530

    Update BubbleSort code

--- sorting/BubbleSort.java
+++ sorting/BubbleSort.java
@@ -26,6 +26,7 @@ public class BubbleSort {
     System.out.print("Enter the size of an array: ");
     int n = sc.nextInt();
     int[] arr = new int[n];
+    System.out.print("Enter " + n + " elements: ");
     for (int i = 0; i < n; i++) {
       arr[i] = sc.nextInt();
     }
