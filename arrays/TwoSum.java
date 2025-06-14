c85cb49cab864f2ba419042fd3b46d9e273d89f0
Sujal Dudhe <sujaldudhe2004@gmail.com>
2025-06-14 17:24:08 +0530

    Add Two Sum code

---
+++ arrays/TwoSum.java
@@ -0,0 +1,34 @@
+package arrays;
+import java.util.Map;
+import java.util.HashMap;
+
+// Given an array of integers nums and an integer target. Return the indices(0 - indexed) of two elements in nums such that they add up to target.
+public class TwoSum {
+
+  public static int[] twoSum(int[] nums, int target) {
+    Map<Integer, Integer> mp = new HashMap<>();
+
+    for (int i = 0; i < nums.length; i++) {
+      int num = nums[i];
+
+      int moreNeeded = target - num;
+
+      if (mp.containsKey(moreNeeded)) {
+        return new int[] {mp.get(moreNeeded), i};
+      }
+
+      mp.put(num, i);
+    }
+
+    return new int[] {-1, -1};
+  }
+
+  public static void main(String[] args) {
+    int[] arr = {1, 3, 5, -7, 6, -3};
+    int target = 0;
+
+    int[] ans = twoSum(arr, target);
+
+    System.out.println("This is the answer: [" + ans[0] + ", " + ans[1] + "]");
+  }
+}
